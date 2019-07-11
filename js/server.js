ef9dc3626219ecbe083a32f367bc96586024f7f2
MBENDER MBENDER <margarita.bender@direkt-gruppe.de>
2019-07-11 10:04:34 +0200

    Implement UAA-service

--- js/server.js
+++ js/server.js
@@ -1,14 +1,42 @@
 /*eslint no-console: 0, no-unused-vars: 0*/
 "use strict";
-
+//https://github.com/SAPDocuments/Tutorials/blob/master/tutorials/xsa-node-modules/xsa-node-modules.md
+ 
+var https = require("https");
 var xsjs = require("@sap/xsjs");
 var xsenv = require("@sap/xsenv");
 var port = process.env.PORT || 30000;
+var server = require("http").createServer();
+
+https.globalAgent.options.ca= xsenv.loadCertificates(); 	
+
+global.__base = __dirname + "/";
+var init = require(global.__base + "utils/initialize");
+
+//Initialize Express App for XSA UAA and HDBEXT Middleware
+var app = init.initExpress();
+
+
+//Setup Routes
+var router = require("./router")(app, server);
+
+//Initialize the XSJS Compatibility Layer
+init.initXSJS(app);
+
+
+//Start the Server
+server.on("request", app);
+
+server.listen(port, function() {
+	console.info("HTTP Server: " + server.address().port);
+});
 
 var options = {
 	anonymous: true, // remove to authenticate calls
 	redirectUrl: "/index.xsjs"
 };
+
+
 
 // configure HANA
 try {
